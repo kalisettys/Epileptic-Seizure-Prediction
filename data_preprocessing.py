4bb1078985532fba325978d55b2b26a130b068dc
Shilpa Kalisetty <kalisettys@vcu.edu>
2019-04-27 08:09:30 -0400

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -13,15 +13,12 @@ def preprocess(filename="project635/case01/chb01_01.edf"):
     num_of_rows = 0
     csv_output_file = filename.split(".")[0] + ".csv"
 
-    with open(csv_output_file, "w") as output_file:
+    with open(csv_output_file,"w") as output_file, open("project635/case01/patient_input.csv") as input_file:
+        #saves all the 432 features to a csv for all 18 channels
         write_to_csv = csv.writer(output_file, delimiter=",")
 
-    with open("project635/case01/patient_input.csv") as input_file:
+        #read input_file file with seizure data about each edf file
         read_csv = csv.reader(input_file, delimiter=",")
-
-
-
-
         f = pyedflib.EdfReader(filename)
         n = f.signals_in_file
 
@@ -40,16 +37,11 @@ def preprocess(filename="project635/case01/chb01_01.edf"):
 
         # looking at only the 6 seconds of the entire time period and going by 2 second epochs
         for start_point in range(0,len(num_of_channels[0])-(data_points_per_second*num_of_windows*size_per_window), data_points_per_second):
-
             list_of_features = []
-
 
             #appending 3, 2 sec windows in window holder
             for channel in num_of_channels:
-
                 first_six_seconds = channel[start_point: start_point + (data_points_per_second * num_of_windows * size_per_window)]
-                #input(len(first_six_seconds))
-
                 two_second_windows = np.array_split(first_six_seconds, 3)
 
                 ##3 lists each with 24 values for 2 second windows
@@ -60,7 +52,6 @@ def preprocess(filename="project635/case01/chb01_01.edf"):
                     windows_with_fft.append(perform_fft)
 
                 #input(len(windows_with_fft))
-
 
                 for window_with_fft in windows_with_fft:
                     each_block_points = np.array_split(window_with_fft, 8)
@@ -76,30 +67,17 @@ def preprocess(filename="project635/case01/chb01_01.edf"):
     print("Total Lines: {0}".format(num_of_rows))
 
 
-####read csv files
+all_files = os.listdir("project635/case01")
 
+myFiles = []
+
+for each_file in os.listdir("project635/case01"):
+
+    if each_file.endswith(".edf"):
+        x = os.path.join("project635/case01", each_file)
+        myFiles.append(x)
+
+for file in sorted(myFiles):
+    print(file)
 
 preprocess()
-
-
-
-
-
-
-
-
-
-
-
-# all_files = os.listdir("project635/case01")
-#
-# myFiles = []
-#
-# for each_file in os.listdir("project635/case01"):
-#
-#     if each_file.endswith(".edf"):
-#         x = os.path.join("project635/case01", each_file)
-#         myFiles.append(x)
-#
-# for file in sorted(myFiles):
-#     print(file)
