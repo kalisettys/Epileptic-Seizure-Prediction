771fa5e86d116bb47c40b360353c9c8a68fe9df9
Shilpa Kalisetty <kalisettys@vcu.edu>
2019-04-27 21:49:25 -0400

    Update classifier_models.py

--- classifier_models.py
+++ classifier_models.py
@@ -1,58 +1,73 @@
 import numpy as np
-import pandas as pd
 
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 
-
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-
-from sklearn.model_selection import StratifiedKFold
-
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+
+import csv
+
+
+## loading training dataset
+with open("project635/case01/Training.csv", newline='\n') as training_file:
+    training_data = list(csv.reader(training_file))
+
+    x_train = np.array_split(training_data, [432], axis=1)
+    y_train = x_train[1]
+    #input(y_train[10179:10185])
+
+
+# ## loading testing dataset
+with open("project635/case01/Testing.csv", newline='') as testing_file:
+    testing_data = list(csv.reader(testing_file))
+    x_test = np.array_split(testing_data, [432], axis = 1)
+    y_test = x_test[1]
 
 
 
-##splitting data into training and test data sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 0)
-
-## feature scaling
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-
-## confusion matrix
-
-#confusionMatrix = confusion_matrix(y_test, y_pred=)
-#print(confusionMatrix)
-
-
-## 5-fold cross validation
-seed = 7
-np.random.seed(seed)
-kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
-
-##SVM
-svm = SVC(kernel="linear", c = 0.025, random_state=101)
-#svm.fit(x_train, y_train)
-#y_pred = svm.predict(x_test)
-
-
-
+#
+#
+#
+# ## confusion matrix
+#
+# #confusionMatrix = confusion_matrix(y_test, y_pred=)
+# #print(confusionMatrix)
+#
+#
+# ## 5-fold cross validation
+# seed = 7
+# np.random.seed(seed)
+# kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
+#
+# ##SVM
+# svm = SVC(kernel="rbf", c = 1.0, random_state=101)
+# #svm.fit(x_train, y_train)
+# #y_pred = svm.predict(x_test)
+#
+#
+#
 ## K-Nearest Neighbors
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(x_train, y_train)
+y_true_value = [x_test, y_test]
+y_predict = knn.predict(x_test)
 
-knn = KNeighborsClassifier(n_neighbors=15)
-#knn.fit(x_train, y_train)
-#y_pred = knn.predict(x_test)
+#Classification Report
+target_name = ['class 0', 'class 1']
+print(classification_report(y_true_value, y_predict, target_name))
 
 
 
-## Decision Trees
 
-dtree = DecisionTreeClassifier(max_depth=10, random_state=101, max_features=None, min_samples_leaf=15)
 
-#dtree.fit(x_train, y_train)
+
+#
+# ## Decision Trees
+#
+# dtree = DecisionTreeClassifier(max_depth=10, random_state=101, max_features=None, min_samples_leaf=15)
+#
+# #dtree.fit(x_train, y_train)
 #y_pred = dtree.predict(x_test)
